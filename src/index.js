99b9bdb491a83c513a32827eb6ee5d81fd6e72e1
Daniel Sanchez <danxschz@gmail.com>
2022-05-20 15:36:00 -0400

    Reorganize script and style.scss
    - Reorganize code to be easier to read and understand
    - Set clearer names for variables and selectors

--- src/index.js
+++ src/index.js
@@ -2,60 +2,50 @@ import './normalize.css';
 import './style.scss';
 import moment from 'moment';
 
-const toDo = (title, dueDate = null, priority = 'normal', complete = false) => {
-  return {title, dueDate, priority, complete};
+const ToDo = (description, dueDate, priority = 'normal', complete = false) => {
+  return {description, dueDate, priority, complete};
 }
 
-const list = (name) => {
-  const content = [];
-
-  const changeCompleteStatus = (index) => {
-    if (content[index].complete === true) {
-      content[index].complete = false;
-    } else {
-      content[index].complete = true;
-    }
-  }
+const Project = (name) => {
+  const list = [];
 
   const appendToDo = (toDo) => {
-    content.push(toDo);
+    list.push(toDo);
   }
 
   const removeToDo = (i) => {
-    content.splice(i, 1);
+    list.splice(i, 1);
   }
 
-  const changePriority = (i) => {
-    if (content[i].priority === 'normal') {
-      content[i].priority = 'important';
+  const changeCompleteStatus = (i) => {
+    if (list[i].complete === true) {
+      list[i].complete = false;
     } else {
-      content[i].priority = 'normal';
+      list[i].complete = true;
     }
   }
 
-  return {name, content, changeCompleteStatus, appendToDo, removeToDo, changePriority};
+  const changePriority = (i) => {
+    if (list[i].priority === 'normal') {
+      list[i].priority = 'important';
+    } else {
+      list[i].priority = 'normal';
+    }
+  }
+
+  return {name, list, appendToDo, removeToDo, changeCompleteStatus, changePriority};
 }
 
-// Default list
-const homeList = list('home');
-let test1 = toDo('Clean room', '2022-05-11', 'normal');
-let test2 = toDo('Learn react', '2022-05-19', 'important');
-let test3 = toDo('Work out', '2022-05-14', 'normal');
-homeList.appendToDo(test1);
-homeList.appendToDo(test2);
-homeList.appendToDo(test3);
-
-const displayList = (list) => {
-  let listContainer = document.querySelector('.list');
+const displayList = (project) => {
+  let list = document.querySelector('.list');
   let i = 0;
-  for (let item of list.content) {
+  for (let item of project.list) {
     let toDo = document.createElement('div');
     toDo.classList.add('to-do');
-    toDo.setAttribute('data-index', i);
-    i++;
+    toDo.setAttribute('data-index', i++); // Keep an eye for this change
 
-    let checkboxContainer = document.createElement('div');
-    checkboxContainer.classList.add('to-do__checkbox');
+    let checkboxDiv = document.createElement('div');
+    checkboxDiv.classList.add('to-do__checkbox');
 
     let checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
@@ -63,23 +53,21 @@ const displayList = (list) => {
       checkbox.checked = true;
       toDo.classList.add('complete');
     }
-    checkboxContainer.appendChild(checkbox);
+    checkboxDiv.appendChild(checkbox);
+    toDo.appendChild(checkboxDiv);
 
-    toDo.appendChild(checkboxContainer);
-
-    let informationContainer = document.createElement('div');
-    informationContainer.classList.add('to-do__information');
-
-    let title = document.createElement('div');
-    title.classList.add('to-do__title');
+    let contentDiv = document.createElement('div');
+    contentDiv.classList.add('to-do__content');
 
     if (item.priority === 'important') {
-      checkboxContainer.classList.add('important');
-      informationContainer.classList.add('important');
+      checkboxDiv.classList.add('important');
+      contentDiv.classList.add('important');
     }
 
-    title.textContent = item.title;
-    informationContainer.appendChild(title);
+    let description = document.createElement('div');
+    description.classList.add('to-do__description');
+    description.textContent = item.description;
+    contentDiv.appendChild(description);
 
     let date = document.createElement('div');
     date.classList.add('to-do__date');
@@ -91,14 +79,10 @@ const displayList = (list) => {
       lastWeek: '[Last] dddd',
       sameElse: 'DD/MM/YYYY'
     });
-    informationContainer.appendChild(date);
+    contentDiv.appendChild(date);
 
     let icons = document.createElement('div');
     icons.classList.add('icons');
-
-    let dateIcon = document.createElement('i');
-    dateIcon.setAttribute('class', 'fa-solid fa-calendar');
-    icons.appendChild(dateIcon);
 
     let priorityIcon = document.createElement('i');
     priorityIcon.setAttribute('class', 'fa-solid fa-triangle-exclamation');
@@ -109,67 +93,73 @@ const displayList = (list) => {
     deleteIcon.setAttribute('class', 'fa-solid fa-trash');
     icons.appendChild(deleteIcon);
 
-    informationContainer.appendChild(icons);
-
-    toDo.appendChild(informationContainer);
-
-    listContainer.appendChild(toDo);
+    contentDiv.appendChild(icons);
+    toDo.appendChild(contentDiv);
+    list.appendChild(toDo);
   }
 }
 
 const clearDisplay = () => {
-  let listItems = document.querySelectorAll('.list div');
-  listItems.forEach(item => {
+  let listItems = document.querySelectorAll('.to-do');
+  listItems.forEach((item) => {
     item.remove();
   });
 }
 
-displayList(homeList);
-
-
 const buildToDo = () => {
-  let title = document.querySelector('#title');
-  let date = document.querySelector('#date');
-  return toDo(title.value, date.value);
+  let description = document.querySelector('#description');
+  let date = document.querySelector('#due-date');
+  return ToDo(description.value, date.value);
 }
 
-const submitButton = document.querySelector('button');
-submitButton.addEventListener('click', () => {
-  let title = document.querySelector('#title');
-  if (title.value === '') return;
-  homeList.appendToDo(buildToDo());
-  clearDisplay();
-  displayList(homeList);
-  console.log(homeList.content);
-});
-
-
-// Set event listeners
-let listItems = document.querySelectorAll('.to-do');
-listItems.forEach(item => {
-  let i = item.getAttribute('data-index');
-
-  let checkbox = item.querySelector('.to-do__checkbox input');
-  checkbox.addEventListener('click', () => {
-    homeList.changeCompleteStatus(i);
-    console.log(homeList.content);
-    item.classList.toggle('complete');
-  });
-
-  let deleteIcon = item.querySelector('.fa-trash');
-  deleteIcon.addEventListener('click', () => {
-    homeList.removeToDo(i);
+const setAddToDoEvent = (project) => {
+  let addToDoButton = document.querySelector('.to-do-form__button');
+  addToDoButton.addEventListener('click', () => {
+    let description = document.querySelector('#description');
+    if (description.value === '') return;
+    project.appendToDo(buildToDo());
     clearDisplay();
-    displayList(homeList);
+    displayList(project);
   });
+}
 
-  let priorityIcon = item.querySelector('.fa-triangle-exclamation');
-  priorityIcon.addEventListener('click', ()=> {
-    homeList.changePriority(i);
-    clearDisplay();
-    displayList(homeList);
+const setToDoEvents = (project) => {
+  let listItems = document.querySelectorAll('.to-do');
+  listItems.forEach((item) => {
+    let i = item.getAttribute('data-index');
+
+    let checkbox = item.querySelector('.to-do__checkbox input');
+    checkbox.addEventListener('click', () => {
+      project.changeCompleteStatus(i);
+      clearDisplay();
+      displayList(project);
+      //item.classList.toggle('complete');
+    });
+
+    let deleteIcon = item.querySelector('.fa-trash');
+    deleteIcon.addEventListener('click', () => {
+      project.removeToDo(i);
+      clearDisplay();
+      displayList(project);
+    });
+
+    let priorityIcon = item.querySelector('.fa-triangle-exclamation');
+    priorityIcon.addEventListener('click', ()=> {
+      project.changePriority(i);
+      clearDisplay();
+      displayList(project);
+    });
   });
-});
+}
 
-let asd = document.querySelector('#date');
-console.log(asd.value);
+// Default project
+const home = Project('home');
+let test1 = ToDo('Walk the dog', '2022-05-16', 'normal');
+let test2 = ToDo('Save the world', '2022-05-21', 'important');
+let test3 = ToDo('Work out', '2022-05-20', 'normal');
+home.appendToDo(test1);
+home.appendToDo(test2);
+home.appendToDo(test3);
+displayList(home);
+setAddToDoEvent(home);
+setToDoEvents(home);
